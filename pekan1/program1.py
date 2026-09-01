428f0cfb0bbea2f893ac804e98e10e1f89f72246
Muhammad Fawwaz <gallagherfawwaz@gmail.com>
2026-09-01 21:23:53 +0700

    first commit

---
+++ pekan1/program1.py
@@ -0,0 +1,12 @@
+#program pertama
+"""
+komentar 
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("hello world")
+print()
+print("program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh \n " + "Muhammad Fawwaz")
